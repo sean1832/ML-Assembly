a1b4e2721a72d7984a498cb5c4d733aba2e69e3e
sean1832 <sean1832725142@gmail.com>
2023-01-06 05:15:34 +1100

    modified: add monobehavior as class extension

--- Timber-Assembly/Assets/Scripts/Traning/_scr/Module/DataIO/CSV_IO.cs
+++ Timber-Assembly/Assets/Scripts/Traning/_scr/Module/DataIO/CSV_IO.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class CSV_IO
+public class CSV_IO : MonoBehaviour
 {
     public static List<string[]> Read(string path)
     {
