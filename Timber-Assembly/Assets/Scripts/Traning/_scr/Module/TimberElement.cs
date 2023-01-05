51d078ed13d873d88e0ce7e675a5b50fba00d863
sean1832 <sean1832725142@gmail.com>
2023-01-06 00:16:39 +1100

    added timber module

---
+++ Timber-Assembly/Assets/Scripts/Traning/_scr/Module/TimberElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string Name { get; set; }
+    public double Length { get; set; }
+    public bool IsFitted { get; set; }
+}
