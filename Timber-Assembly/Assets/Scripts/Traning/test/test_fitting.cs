a191a5e52386402c10db8505b2a266c5cbba7fa8
sean1832 <sean1832725142@gmail.com>
2023-01-06 04:06:52 +1100

    add: retrieve salvage timber data and assign it to list for comparison

--- Timber-Assembly/Assets/Scripts/Traning/test/test_fitting.cs
+++ Timber-Assembly/Assets/Scripts/Traning/test/test_fitting.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class test_fitting : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class test_fitting : MonoBehaviour
 
     private List<TimberElement> _timberTargets = new List<TimberElement>();
     private List<TimberElement> _timberSalvage = new List<TimberElement>();
+    private List<string[]> _timberSalvageData;
 
     private GameObject _stud;
     private GameObject _nogging;
@@ -23,7 +26,8 @@ public class test_fitting : MonoBehaviour
         _frame = gameObject.GetComponent<FramingSystem>();
         Transform framingParent = _frame.framingParent;
         _frameParentObj = framingParent.GameObject();
-        
+        _timberSalvageData = CSV_IO.Read(@"Assets\Data\data.csv"); // csv: (name, length)
+
         _stud = _frame.Stud;
         _nogging = _frame.Nogging;
     }
@@ -38,10 +42,10 @@ public class test_fitting : MonoBehaviour
 
         List<GameObject> timbers = GetChildren(_frameParentObj);
 
-        // assign 
+        // assign target timbers
         foreach (var timber in timbers)
         {
-            double length = System.Math.Round(Mathf.Max(timber.transform.localScale.x, timber.transform.localScale.y), 2);
+            float length = (float)System.Math.Round(Mathf.Max(timber.transform.localScale.x, timber.transform.localScale.y), 2);
             string name = timber.name;
 
             // write metadata to timber element object
@@ -50,19 +54,45 @@ public class test_fitting : MonoBehaviour
                 GameObject = timber,
                 Name = name,
                 Length = length,
-                IsFitted = false
+                IsFitted = false,
+                TimberType = "target"
             };
 
             _timberTargets.Add(element);
         }
 
+        // assign salvaged timbers
+        foreach (var timberData in _timberSalvageData)
+        {
+
+            string name = timberData[0];
+            float length = float.Parse(timberData[1]);
+
+            TimberElement element = new TimberElement()
+            {
+                Name = name,
+                Length = length,
+                TimberType = "salvaged"
+            };
+
+            _timberSalvage.Add(element);
+        }
+
         foreach (var timberTarget in _timberTargets)
         {
-            
+            foreach (var salvagedTimber in _timberSalvage)
+            {
+
+            }
         }
         stop = true;
     }
 
+    private static GameObject CreateObject(string name)
+    {
+        GameObject obj = new GameObject(name);
+        return obj;
+    }
 
     private static List<GameObject> GetChildren(GameObject parent, GameObject filter = null, string filterName = null)
     {
