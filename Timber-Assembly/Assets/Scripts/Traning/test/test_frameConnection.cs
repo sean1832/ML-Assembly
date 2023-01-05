24234bd3e33963c34f655d06287e456ee46acc3f
sean1832 <sean1832725142@gmail.com>
2023-01-06 05:16:41 +1100

    remove error code

--- Timber-Assembly/Assets/Scripts/Traning/test/test_frameConnection.cs
+++ Timber-Assembly/Assets/Scripts/Traning/test/test_frameConnection.cs
@@ -7,9 +7,18 @@ public class test_frameConnection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var frame = gameObject.AddComponent<FramingSystemOptimised>();
-        frame.Openings
-        frame.NewLayout();
+        
+        var frame = gameObject.GetComponent<FramingSystemOptimised>();
+        //frame.NewLayout();
+
+
+        //for (int i = 0; i < frame.TimberLengths.Count; i++)
+        //{
+        //    float timberLength = frame.TimberLengths[i];
+        //    string timberName = frame.TimberNames[i];
+        //    print($"timber name: {timberName}, timber length: {timberLength}");
+        //}
+        //print("");
     }
 
     // Update is called once per frame
