6bfb1f158930a7d510304ca42ec62d049b6061dd
sean1832 <sean1832725142@gmail.com>
2023-01-06 03:55:43 +1100

    change length datatype to float

--- Timber-Assembly/Assets/Scripts/Traning/_scr/Module/TimberElement.cs
+++ Timber-Assembly/Assets/Scripts/Traning/_scr/Module/TimberElement.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TimberElement
 {
+    public GameObject GameObject { get; set; }
     public string Name { get; set; }
-    public double Length { get; set; }
+    public float Length { get; set; }
     public bool IsFitted { get; set; }
 }
