bcf71055f6a900672df66910d9fe7be953801555
sean1832 <sean1832725142@gmail.com>
2023-01-06 02:57:28 +1100

    add script to create dummy data for salvaged timber

---
+++ Timber-Assembly/Assets/Scripts/Traning/_scr/Tools/DummyDataGenerator.cs
@@ -0,0 +1,43 @@
+using System.IO;
+using UnityEngine;
+using System.Text;
+using Random = System.Random;
+using System;
+using UnityEngine.UI;
+
+public class DummyDataGenerator: MonoBehaviour
+{
+    [SerializeField][Range(1,50)]private int _dataAmount = 10;
+    [SerializeField] private string _path = @"Assets\Data\data.csv";
+    [SerializeField] private bool _write = false;
+
+
+    void Update()
+    {
+        if (!_write) return;
+        WriteToCsv();
+        _write = false;
+
+    }
+
+    public void WriteToCsv()
+    {
+        StringBuilder csvBuilder = new StringBuilder();
+        Random random = new System.Random();
+
+        for (int i = 0; i < _dataAmount; i++)
+        {
+            // generate random length between 1 and 6 meter
+            float length = (float)random.NextDouble() * 5 + 1;
+
+            // round by 2
+            length = (float)System.Math.Round(length, 2);
+
+            // add to line
+            csvBuilder.AppendLine($"T{i + 1},{length}");
+        }
+        string[] lines = csvBuilder.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.None); ;
+
+        File.WriteAllLines(_path, lines);
+    }
+}
