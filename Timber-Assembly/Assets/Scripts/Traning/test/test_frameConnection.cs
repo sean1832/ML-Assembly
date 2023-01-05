c2f41ca7c056b87f8921f6c878f7a4478111a62a
sean1832 <sean1832725142@gmail.com>
2023-01-05 16:39:06 +1100

    add test script to connect framing script

---
+++ Timber-Assembly/Assets/Scripts/Traning/test/test_frameConnection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_frameConnection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var frame = gameObject.AddComponent<FramingSystemOptimised>();
+        frame.Openings
+        frame.NewLayout();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
