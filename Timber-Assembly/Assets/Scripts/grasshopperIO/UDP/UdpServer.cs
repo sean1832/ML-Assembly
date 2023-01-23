d20b14939b2e598edbb849770f24e1f4a7ed8203
sean1832 <sean1832725142@gmail.com>
2023-01-24 00:54:25 +1100

    add function for udp send

--- Timber-Assembly/Assets/Scripts/grasshopperIO/UDP/UdpServer.cs
+++ Timber-Assembly/Assets/Scripts/grasshopperIO/UDP/UdpServer.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
 
 public class UdpServer : MonoBehaviour
 {
-    public static void UdpSender()
+    public static void UdpSender(string data, string address, int port)
     {
-
+        using (var client = new UdpClient(port))
+        {
+            byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(data);
+            int dataSize = dataBytes.Length;
+            client.Send(dataBytes, dataSize, address, port);
+        }
     }
 }
