9180da5c841b9ef8b94be192e182b49a08c5f446
sean1832 <sean1832725142@gmail.com>
2023-02-17 12:37:42 +1100

    test: unity<->udp<->grasshopper live link

--- Timber-Assembly/Assets/Scripts/Traning/_scr/Train.cs
+++ Timber-Assembly/Assets/Scripts/Traning/_scr/Train.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 
+
 public class Train : Agent
 {
+    [SerializeField][Range(1,5)] private float wallHeight = 1;
+
+    [SerializeField][Range(5,20)] private float wallLength = 5;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        gameObject.GetComponent<Gh_IO>().msgToGh = $"{wallHeight},{wallLength}";
     }
 }
