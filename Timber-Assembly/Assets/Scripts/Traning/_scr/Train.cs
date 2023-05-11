082a0d2101f70dcbcd9229519a25609df0a3eb72
sean1832 <sean1832725142@gmail.com>
2023-05-11 17:17:18 +1000

    update 11-05-2023

--- Timber-Assembly/Assets/Scripts/Traning/_scr/Train.cs
+++ Timber-Assembly/Assets/Scripts/Traning/_scr/Train.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
@@ -15,6 +16,9 @@ public class Train : Agent
     
     [SerializeField][Range(0.01f, 1.0f)] private float _windowMoveSpeed = 0.1f;
     [SerializeField][Range(0.01f, 0.5f)] private float _windowSizeSpeed = 0.1f;
+    [SerializeField] private bool _export = false;
+
+    private int _token;
 
     private float _wallHeight;
     private float _wallWidth;
@@ -33,6 +37,16 @@ public class Train : Agent
     private string _lastDataFromGh = "";
     private bool _dataFromGhIsChanged = false;
 
+    [System.Serializable]
+    public class InputData
+    {
+        public float WindowPosX;
+        public float WindowPosY;
+        public float WindowSizeX;
+        public float WindowSizeY;
+        public float WallHeight;
+        public float WallWidth;
+    }
 
     [System.Serializable]
     public class GhData
@@ -49,13 +63,46 @@ public class Train : Agent
         public Vector3[] WindowPos;
         public Vector2[] WindowScale;
         public bool[] IsAtBounds;
+        public InputData InputParams;
     }
 
 
     private GhData _ghData;
 
+    private void Start()
+    {
+        // start coroutine to get data from grasshopper
+        StartCoroutine(WaitForGrasshopperDataAndUpdate());
+    }
+    private IEnumerator WaitForGrasshopperDataAndUpdate()
+    {
+        while (true)
+        {
+            // get from grasshopper
+            string dataFromGh = gameObject.GetComponent<Gh_IO>().msgFromGh;
+
+            // check for message changes
+            _previousDataFromGh = _lastDataFromGh;
+            _lastDataFromGh = dataFromGh;
+            if (_lastDataFromGh != _previousDataFromGh)
+            {
+                _dataFromGhIsChanged = true;
+            }
+
+            if (_dataFromGhIsChanged)
+            {
+                _ghData = JsonUtility.FromJson<GhData>(dataFromGh);
+                Academy.Instance.EnvironmentStep();
+                _dataFromGhIsChanged = false;
+            }
+
+            yield return null;
+        }
+    }
+
     public override void OnEpisodeBegin()
     {
+        _token = 5;
         // randomize wall height & length
         _wallWidth = Random.Range(3f, 10f);
         _wallHeight = Random.Range(2.1f, 3.5f);
@@ -115,6 +162,7 @@ public class Train : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        print($"Step: {StepCount} | Reward: {_ghData.Score}");
         // window param
         _windowPosX += actions.ContinuousActions[0] * Time.deltaTime * _windowMoveSpeed;
         _windowPosY += actions.ContinuousActions[1] * Time.deltaTime * _windowMoveSpeed;
@@ -124,41 +172,72 @@ public class Train : Agent
         // send to grasshopper
         gameObject.GetComponent<Gh_IO>().msgToGh = $"{_windowPosX},{_windowPosY},{_windowSizeX},{_windowSizeY},{_wallHeight},{_wallWidth}";
 
-        // get from grasshopper
-        string dataFromGh = gameObject.GetComponent<Gh_IO>().msgFromGh;
+        //// get from grasshopper
+        //string dataFromGh = gameObject.GetComponent<Gh_IO>().msgFromGh;
 
-        // check for message changes
-        _previousDataFromGh = _lastDataFromGh;
-        _lastDataFromGh = dataFromGh;
-        if (_lastDataFromGh != _previousDataFromGh)
-        {
-            _dataFromGhIsChanged = true;
-        }
+        //// check for message changes
+        //_previousDataFromGh = _lastDataFromGh;
+        //_lastDataFromGh = dataFromGh;
+        //if (_lastDataFromGh != _previousDataFromGh)
+        //{
+        //    _dataFromGhIsChanged = true;
+        //}
 
-        // if data is changed
-        if (!_dataFromGhIsChanged) return;
-        _ghData = JsonUtility.FromJson<GhData>(dataFromGh);
+        //// if data not change yet, wait until it changes
+        //if (!_dataFromGhIsChanged)
+        //{
+        //    return;
+        //}
+        //_ghData = JsonUtility.FromJson<GhData>(dataFromGh);
 
         SetReward(_ghData.Score);
+
+        if (_ghData.Score > 600)
+        {
+            AddReward(1000);
+            if (_export && _token > 0)
+            {
+                _token--;
+                ExportParam(_ghData.InputParams);
+                print($"Exported at {_ghData.Score}");
+            }
+            EndEpisode();
+        }
+
         // if any of the window smaller than 200x200, end episode
         if (_ghData.WindowScale.Any(winScale => winScale.x < 0.2f || winScale.y < 0.2f))
         {
-            AddReward(-50);
+            AddReward(-500);
             ResetWindow();
         }
 
         //if any of the window is touching boundary, end episode
         if (_ghData.IsAtBounds.Any(isAtBound => isAtBound))
         {
-            AddReward(-50);
+            AddReward(-500);
             ResetWindow();
         }
 
+        // success condition
         if (_ghData.MinCutRatio >= 90)
         {
-            AddReward(+100);
+            AddReward(+1000);
+            if (_export)
+            {
+                ExportParam(_ghData.InputParams);
+            }
             EndEpisode();
         }
+    }
+
+    // Create a new method for manual stepping
+    public void ManualStep()
+    {
+        // Call RequestDecision for agent's decision-making
+        RequestDecision();
+
+        // Call RequestAction for agent's action execution
+        RequestAction();
     }
 
     public override void Heuristic(in ActionBuffers actionOut)
@@ -170,9 +249,38 @@ public class Train : Agent
 
     private void ResetWindow()
     {
+        print("window reseted");
         _windowPosX = _windowInitPosX;
         _windowPosY = _windowInitPosY;
         _windowSizeX = _windowInitSizeX;
         _windowSizeY = _windowInitSizeY;
+    }
+
+    private void ExportParam(InputData param)
+    {
+        // export param to file
+        string path = "Assets/Results/param.txt";
+
+        // if directory not exist, create directory
+        string dir = Path.GetDirectoryName(path);
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        // create path if not exist
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "");
+        }
+
+        string paramString = $"{param.WindowPosX},{param.WindowPosY},{param.WindowSizeX},{param.WindowSizeY},{param.WallHeight},{param.WallWidth}";
+
+        // append to file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(paramString);
+        writer.Close();
+
+        print("param exported");
     }
 }
